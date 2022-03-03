9688a8132f90c45f6eaba3234cd904469a670a8d
muratbyk05 <95565340+muratbyk05@users.noreply.github.com>
2022-03-03 11:53:52 +0300

    Add files via upload

---
+++ kitle js/code.js
@@ -0,0 +1,36 @@
+function hesap(){
+    let kilo,boy,yaş,sonuc
+    kilo=document.getElementById("kilo")
+    boy=document.getElementById("boy")
+    yaş=document.getElementById("yaş")
+    sonuc=document.getElementById("sonuc")
+   
+ 
+    if(yaş>15){
+        
+        sonuc.innerHTML="geçtiniz"
+      
+    }
+    else{
+        sonuc.innerHTML="yemeksefakir"
+    }
+    
+  
+    
+   /*
+    if(sonuc>18)
+    {
+    alert("111111111111");
+    }
+    else
+    {
+    alert("0000000");
+    }
+      */
+        
+          
+        
+    
+    
+    
+}
